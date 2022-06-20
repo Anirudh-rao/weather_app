9d7fef607998b6262fc2481d1a28378a4c7b2250
Anirudh-rao <anirudh131299@gmail.com>
2022-06-20 18:03:07 +0530

    Added Some Code in HomScreen.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_weather_app/screen/home_screen.dart';
+import 'package:flutter_weather_app/screens/home_screen.dart';
 
 void main() {
   runApp(MyApp());
